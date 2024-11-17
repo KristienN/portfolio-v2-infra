e9f2847c217ad9e3d30f4d50e78b24170b793ab4
KristienN <knyamutsaka@outlook.com>
2024-11-17 13:43:41 +0000

    add deploy job

--- main.tf
+++ main.tf
@@ -9,7 +9,14 @@ terraform {
   required_version = "~> 1.9.8"
 }
 
-provider "digitalocean" {}
+provider "digitalocean" {
+  token = var.digitalocean_access_token
+}
+
+resource "digitalocean_ssh_key" "portfolio-v2-server" {
+  name = "digitalocean_ssh_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
 resource "digitalocean_droplet" "portfolio-v2-server" {
   image  = "ubuntu-24-10-x64"
@@ -17,18 +24,51 @@ resource "digitalocean_droplet" "portfolio-v2-server" {
   region = var.region
   size   = var.size
   ssh_keys = [var.ssh_fingerprint]
-  tags = ["portfolio-v2", "serer"]
+  tags = ["portfolio-v2", "server"]
 
   provisioner "remote-exec" {
     connection {
       type = "ssh"
       user = "root"
+      private_key = file("~/.ssh/id_rsa")
       host = self.ipv4_address
     }
 
-    inline = [
-      "docker pull registry.digitalocean.com/kristien-docr/portfolio-v2:latest",
-      "docker run -d -p 80:80 --name portfolio-v2 registry.digitalocean.com/kristien-docr/portfolio-v2:latest"
-    ]
+    script = "./scripts/install.sh"
+  }
+}
+
+resource "digitalocean_firewall" "portfolio-v2-server" {
+  name = "portfolio-v2-firewall"
+
+  droplet_ids = [digitalocean_droplet.portfolio-v2-server.id]
+
+  inbound_rule {
+    protocol   = "tcp"
+    port_range = "22"
+    source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
+  }
+
+  inbound_rule {
+    protocol   = "tcp"
+    port_range = "all"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol   = "tcp"
+    port_range = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol   = "udp"
+    port_range = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
